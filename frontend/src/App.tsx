43bcfc29b1638a31e0ddd2072cd083d353b39022
zangetsu <dimongames6@gmail.com>
2026-01-05 17:45:00 +0300

    prepare front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import {useState, Suspense} from 'react'
 import './App.css'
+import {Routes, Route, BrowserRouter} from "react-router-dom";
+import Loader from "./ui/loaders/Loader.tsx";
+import Layout from "./ui/components/Layout.tsx";
+import Home from "./ui/pages/Home.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <BrowserRouter>
+                <Suspense fallback={<Loader/>}>
+                    <Routes>
+                        <Route path="/" element={<Layout/>}>
+                            <Route index element={<Home/>}/>
+                        </Route>
+                    </Routes>
+                </Suspense>
+            </BrowserRouter>
+        </>
+    )
 }
 
 export default App
