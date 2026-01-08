088a653e461cc98318d0a24145a341545c5e1135
zangetsu <dimongames6@gmail.com>
2026-01-08 15:36:43 +0300

    series refactor

--- backend/src/lessons/application/use_cases/confirm_lesson.py
+++ backend/src/lessons/application/use_cases/confirm_lesson.py
@@ -1,23 +1,21 @@
-import datetime
-
-from src.lessons.presentation.dependencies import LessonUoWDeps
 from src.users.domain.entities import UserUpdate, User
 from src.users.presentation.dependencies import UserUoWDeps
 from src.utils.datetimes import is_yesterday_two_dates, is_today
 
 
 async def update_series(uow: UserUoWDeps, user: User) -> User:
-    if not user.updated_at:  # нет даты последнего обновления
-        user_data = UserUpdate(id=user.id, series_days=1)
+    series = 1
+
+    if not user.updated_at:
+        series = 1
 
     elif is_today(user.updated_at.date()):  # сегодняшняя ничего не делаем
         return user
 
     elif is_yesterday_two_dates(user.updated_at.date()):  # вчера был пройден урок
-        user_data = UserUpdate(id=user.id, series_days=user.series_days + 1)
+        series += 1
 
-    else:
-        user_data = UserUpdate(id=user.id, series_days=1)
+    user_data = UserUpdate(id=user.id, series_days=series)
 
     async with uow:
         user = await uow.users.update(user_data)
