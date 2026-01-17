77376dcfbd0f163c371e181602aa48afbf380b0f
zangetsu <dimongames6@gmail.com>
2026-01-17 21:36:05 +0300

    fix certbot

--- infra/certbot/generate-certificate.sh
+++ infra/certbot/generate-certificate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # чистим папку, где могут находиться старые сертификаты
-rm -rf /etc/letsencrypt/live/certfolder*
+rm -rf /etc/letsencrypt/live/${DOMAIN}
 
 # выдаем себе сертификат (обратите внимание на переменные среды)
 certbot certonly --standalone --email $DOMAIN_EMAIL -d $DOMAIN --cert-name=certfolder --key-type rsa --agree-tos
@@ -10,5 +10,5 @@ rm -rf /etc/nginx/cert.pem
 rm -rf /etc/nginx/key.pem
 
 # копируем сертификаты из образа certbot в папку Nginx
-cp /etc/letsencrypt/live/certfolder*/fullchain.pem /etc/nginx/cert.pem
-cp /etc/letsencrypt/live/certfolder*/privkey.pem /etc/nginx/key.pem
+cp /etc/letsencrypt/live/${DOMAIN}/fullchain.pem /etc/nginx/cert.pem
+cp /etc/letsencrypt/live/${DOMAIN}/privkey.pem /etc/nginx/key.pem
