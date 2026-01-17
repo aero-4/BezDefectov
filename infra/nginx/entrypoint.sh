ebbb810dc55a36ca92625519a3e6abc2e484d195
zangetsu <dimongames6@gmail.com>
2026-01-17 14:51:33 +0300

    docker container nginx visible dist dir, fixed production dockers containers

--- infra/nginx/entrypoint.sh
+++ infra/nginx/entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 set -e
 
-if [ -z "$DOMAIN" ]; then
-  echo "ERROR: DOMAIN is not set. Provide DOMAIN via .env or environment."; exit 1
+# Если ENVIRONMENT не задан, считаем, что это production-режим
+: "${ENVIRONMENT:=production}"
+
+if [ "$ENVIRONMENT" = "production" ]; then
+    cp /etc/nginx/nginx.prod.conf.template /etc/nginx/nginx.conf
+elif [ "$ENVIRONMENT" = "testing" ]; then
+    cp /etc/nginx/nginx.test.conf.template /etc/nginx/nginx.conf
+else
+    cp /etc/nginx/nginx.dev.conf.template /etc/nginx/nginx.conf
 fi
 
-envsubst '$DOMAIN' \
-  < /etc/nginx/templates/nginx.conf.template \
-  > /etc/nginx/nginx.conf
+# Подстановка переменной DOMAIN (и других, если нужно)
+envsubst '$DOMAIN $API_DOMAIN' < /etc/nginx/nginx.conf > /etc/nginx/nginx.conf.tmp && mv /etc/nginx/nginx.conf.tmp /etc/nginx/nginx.conf
 
 exec nginx -g 'daemon off;'
